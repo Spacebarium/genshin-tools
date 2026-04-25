3a48786692621030c8c4f480f58bc459d0dfe18b
Jared Tham <jared.tham@hotmail.com>
2026-04-25 22:04:40 +0800

    fix: use HashRouter for nav

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "@/App.tsx";
 
 import "@/styles/fonts.css";
@@ -10,8 +10,8 @@ import "@/styles/style.css";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <BrowserRouter basename="/genshin-tools">
+        <HashRouter>
             <App />
-        </BrowserRouter>
+        </HashRouter>
     </StrictMode>,
 );
